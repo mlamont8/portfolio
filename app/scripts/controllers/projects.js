c165c28ede8b962546afb4dd2e4138b531b01944
mlamont8 <debiandmiat@hotmail.com>
2017-07-26 21:05:54 -0400

    updated files

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -4,17 +4,13 @@ angular
 .controller('projectsController', function(){
     var projectsList = this;
     projectsList.data =[{
+        title:'Whats Out – Movies/Tv Shows', image:'/images/projects/whats-out.png', github: 'https://github.com/mlamont8/react-entertainment-search', desc: 'I decided to refactor my previous project from AngularJS to React.  With a few design changes, this was created using Bootstrap for responsiveness.  It calls the API from themoviedb.org and uses the resulting JSON to make Tv Show and Movie pages.  This also features a search feature for each category and a dynamic front page on page refreshes.', link: 'http://mlamont8.site', tech:'React, React-Bootstrap, Sass'
+    },{
+        title:'Better Weather', image:'/images/projects/better-weather.png', github: 'https://github.com/mlamont8/better-weather', desc: 'Another fine weather app built with React along with Bootstrap for responsiveness.  Using the API from Weather Underground, this app provides current weather information along with a 4 day forecast.  Search provided using a combination of the Google Places API and the Weather Underground.', link: 'https://better-weather-4407d.firebaseapp.com/', tech:'React, Sass, React-Bootstrap'
+    },{
         title:'Movie/TV Entertainment search', image:'/images/projects/entsearch.png', github: 'https://github.com/mlamont8/entertainment-search', desc: 'This Entertainment search app was created with Yeoman AngularJS using the ui-router.  It takes the API from themoviedb.org and showcases popular and upcoming movies and TV shows.  Inner pages feature a search option based on user input.  CSS is built with SASS using the Materializecss framework.', link: 'https://movie-and-tv-search.firebaseapp.com/',tech: 'HTML5 ,Angularjs, Materializecss'
     },{
-        title:'Flickr Search', image:'/images/projects/flickrsearch.png', github: 'https://github.com/mlamont8/flickrSearch', desc: 'Using Yeoman Angular as a starting point, this AngularJS app was built using the API from Flickr.com.  This app provides search results for user submitted search terms.  For CSS, Angular Material was used with SASS. The Angular Grid module was used to bring a masonry experience with the results.', link: 'https://simple-flickr-search.firebaseapp.com/', tech:'AngularJS, HTML, Angular Material'
-    },{
-        title:'current weather app', image:'/images/projects/weatherapp.png', github: 'https://github.com/mlamont8/local-weather', desc: 'This online weather app makes use of the API from OpenWeatherMap.com.  Built with vanilla Javascript and jQuery, it uses the browser\'s geolocation information to show weather conditions in the user\'s location.  The app features a Celsius/Fahrenheit toggle and the background image changes based on current weather conditions.', link: 'http://codepen.io/mlamont8/full/pjjvqB/', tech:'HTML5, Bootstrap, CSS, jQuery'
-    },{
         title:'WIKI SEARCH', image:'/images/projects/wikisearch.png', github: 'https://github.com/mlamont8/wiki-search', desc: 'Uses the Wikipedia API to receive results based on local user inputs.  Includes autocomplete and a random result option based on search terms. Built using Bootstrap for responsiveness as well as jQuery and Javascript.', link: 'https://wikisearch-acecd.firebaseapp.com/', tech:'HTML5, Bootstrap, CSS, jQuery'
-    },{
-        title:'POMODORO TIMER', image:'/images/projects/pomodoroClock.png', github: 'https://github.com/mlamont8/Pomodoro-clock', desc: 'A Pomodoro Timer built with Javascript and jQuery.  Session and Reset lengths can be set by the user.  Also includes pause and reset buttons for the timer.', link: 'http://codepen.io/mlamont8/full/meOmEy/', tech:'HTML5, Bootstrap, CSS, jQuery'
-    },{
-        title:'TWITCH USER STATUS', image:'/images/projects/twitchstatus.png', github: 'https://github.com/mlamont8/twitch-online-status', desc: 'Uses the API from Twitch.tv to pull the online status for predefined users.  Organizes users in Online, Offline and Deleted status.  Selecting a result takes you to the user page on Twitch.tv.  Made with Javascript, jQuery, HTML and Bootstrap for responsiveness.', link: 'http://codepen.io/mlamont8/full/bEQOQj/', tech:'HTML5, CSS, jQuery'
     }
     ];
     
